8f37a0ca2f3cfb95e5f8290bf12c5f431354676d
Jon Shipman <jon@jonshipman.com>
2023-01-20 21:45:18 -0600

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { createScene } from '../scene/scene.js';
+
+	let element: HTMLCanvasElement;
+
+	onMount(() => {
+		createScene(element);
+	});
+</script>
+
+<canvas class="center" bind:this={element} />
+
+<style>
+	canvas {
+		display: block;
+		width: 640px;
+		height: 480px;
+	}
+</style>
